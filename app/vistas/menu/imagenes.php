2fdd5173dadab23f6426e0d206a56b73fcf5565a
robertomiguel <robertomiguel@outlook.com>
2017-09-14 10:32:28 -0300

    precios gal img

--- app/vistas/menu/imagenes.php
+++ app/vistas/menu/imagenes.php
@@ -3,56 +3,56 @@
 		<div>
 			<h3 class="letra-premier">
 				<span class="titulo1">TOYOTA HILUX</span>
-				<span class="titulo2">$ 435.900</span>
+				<span class="titulo2">$ 542.800</span>
 			</h3>
 			<img class="auto img-responsive" src="/img/autos/a2016/t1.png" alt="" />
 		</div>
 		<div>
 			<h3 class="letra-premier">
 				<span class="titulo1">TOYOTA HILUX</span>
-				<span class="titulo2">$ 435.900</span>
+				<span class="titulo2">$ 542.800</span>
 			</h3>
 			<img class="auto img-responsive" src="/img/autos/a2016/t2.jpg" alt="" />
 		</div>
 		<div>
 			<h3 class="letra-premier">
 				<span class="titulo1">TOYOTA HILUX</span>
-				<span class="titulo2">$ 435.900</span>
+				<span class="titulo2">$ 542.800</span>
 			</h3>
 			<img class="auto img-responsive" src="/img/autos/a2016/t3.jpg" alt="" />
 		</div>
 		<div>
 			<h3 class="letra-premier">
 				<span class="titulo1">PEUGEOT PARTNER</span>
-				<span class="titulo2">$ 318.500</span>
+				<span class="titulo2">$ 345.800</span>
 			</h3>
 			<img class="auto img-responsive" src="/img/autos/a2016/p1.jpg" alt="" />
 		</div>
 		<div>
 			<h3 class="letra-premier">
 				<span class="titulo1">PEUGEOT PARTNER</span>
-				<span class="titulo2">$ 318.500</span>
+				<span class="titulo2">$ 345.800</span>
 			</h3>
 			<img class="auto img-responsive" src="/img/autos/a2016/p2.jpg" alt="" />
 		</div>
 		<div>
 			<h3 class="letra-premier">
 				<span class="titulo1">PEUGEOT PARTNER</span>
-				<span class="titulo2">$ 318.500</span>
+				<span class="titulo2">$ 345.800</span>
 			</h3>
 			<img class="auto img-responsive" src="/img/autos/a2016/p3.jpg" alt="" />
 		</div>
 		<div>
 			<h3 class="letra-premier">
 				<span class="titulo1">FIAT TORO</span>
-				<span class="titulo2">$ 490.300</span>
+				<span class="titulo2">$ 511.000</span>
 			</h3>
 			<img class="auto img-responsive" src="/img/autos/a2016/f1.jpg" alt="" />
 		</div>
 		<div>
 			<h3 class="letra-premier">
 				<span class="titulo1">FIAT TORO</span>
-				<span class="titulo2">$ 490.300</span>
+				<span class="titulo2">$ 511.000</span>
 			</h3>
 			<img class="auto img-responsive" src="/img/autos/a2016/f2.jpg" alt="" />
 		</div>
